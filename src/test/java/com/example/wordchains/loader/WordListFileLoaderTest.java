ce7bc4f1578666a689895f7fba92953dfc8bdb2d
Hoobie <hoobie92@gmail.com>
2017-08-23 21:47:37 +0200

    Introduce tests for WordListFileLoader, add a dependency for AssertJ

---
+++ src/test/java/com/example/wordchains/loader/WordListFileLoaderTest.java
@@ -0,0 +1,31 @@
+package com.example.wordchains.loader;
+
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WordListFileLoaderTest {
+
+    @Test
+    public void shouldLoadWordListFromFile() throws Exception {
+        // given
+        WordListLoader loader = new WordListFileLoader();
+
+        // when
+        Set<String> wordList = loader.load();
+
+        // then
+        assertThat(wordList)
+                .isNotEmpty()
+                .contains("cat")
+                .contains("cot")
+                .contains("cog")
+                .contains("dog")
+                .contains("lead")
+                .contains("load")
+                .contains("goad")
+                .contains("gold");
+    }
+}
