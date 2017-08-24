a2bcbf73df7812c04ccc35f7064b2dcdd9dea0bc
Hoobie <hoobie92@gmail.com>
2017-08-24 09:29:25 +0200

    Add tests for the solver

--- src/test/java/com/example/wordchains/solver/SimpleSolverTest.java
+++ src/test/java/com/example/wordchains/solver/SimpleSolverTest.java
@@ -2,24 +2,36 @@ package com.example.wordchains.solver;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static com.example.wordchains.assertions.WordChainAssert.assertThat;
 
 public class SimpleSolverTest {
 
+    private static final Solver SOLVER = new SimpleSolver();
+
     @Test
     public void shouldReturnPossiblyNotOptimalSolution() throws Exception {
-        // given
-        Solver solver = new SimpleSolver();
-
-        // when
-        List<String> chain = solver.buildChain("cat", "dog");
-
-        // then
-        assertThat(chain)
+        assertThat(SOLVER.buildChain("cat", "dog"))
                 .isCorrect()
                 .startsWith("cat")
                 .endsWith("dog");
+
+        assertThat(SOLVER.buildChain("lead", "gold"))
+                .isCorrect()
+                .startsWith("lead")
+                .endsWith("gold");
+
+        assertThat(SOLVER.buildChain("ruby", "code"))
+                .isCorrect()
+                .startsWith("ruby")
+                .endsWith("code");
+    }
+
+    @Test
+    public void shouldReturnOptimalSolution() throws Exception {
+        assertThat(SOLVER.buildChain("cat", "dog")).hasSize(4);
+
+        assertThat(SOLVER.buildChain("lead", "gold")).hasSize(4);
+
+        assertThat(SOLVER.buildChain("ruby", "code")).hasSize(6);
     }
 }
