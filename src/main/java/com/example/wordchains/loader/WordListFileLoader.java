5f9f5db55930940f0066082afbff7d1986e7bd34
Hubert Slojewski <hubert.slojewski@tesco.com>
2019-12-24 11:49:25 +0100

    Minor cleanup

--- src/main/java/com/example/wordchains/loader/WordListFileLoader.java
+++ src/main/java/com/example/wordchains/loader/WordListFileLoader.java
@@ -4,11 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,15 +15,12 @@ public class WordListFileLoader implements WordListLoader {
 
     @Override
     public Set<String> load() {
-        try {
-            try (InputStream is = getClass().getResourceAsStream(WORD_LIST_FILE_NAME)) {
-                if (is == null) return null;
-                try (InputStreamReader isr = new InputStreamReader(is);
-                     BufferedReader reader = new BufferedReader(isr)) {
-                    return reader.lines().collect(Collectors.toCollection(HashSet::new));
-                }
+        try (InputStream is = getClass().getResourceAsStream(WORD_LIST_FILE_NAME)) {
+            if (is == null) return new HashSet<>();
+            try (InputStreamReader isr = new InputStreamReader(is); BufferedReader reader = new BufferedReader(isr)) {
+                return reader.lines().collect(Collectors.toCollection(HashSet::new));
             }
-        } catch (IOException  e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return Collections.emptySet();
